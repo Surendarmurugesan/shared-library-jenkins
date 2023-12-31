03e33c3322a9c794702ccc00dc7e75a5ae953bad
SUREMUR <surendar.murugesan@mercedes-benz.com>
2023-12-31 12:45:44 +0100

    groovy script for git checkout, docker build, cleanup

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,16 @@
+def call(String project, String hubUser){
+    sh "docker image build -t ${hubUser}/${project} ."
+    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
+    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+
+    withCredentials([usernamePassword(
+        credentialId: "docker_admin",
+        usernameVariable: "USER",
+        passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"   
+}
